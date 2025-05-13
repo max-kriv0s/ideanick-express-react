7b50c3f629b68aeaea534b7e6304aca2b20047cc
Maxim Krivoshein <max.kriv0s@gmail.com>
2025-05-14 01:29:34 +0300

    feat: add trpc for backend and frontend

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,32 +1,10 @@
+import { TrpcProvider } from './lib/trpc';
+import { AllIdeasPage } from './pages/AllIdeaPage';
+
 export const App = () => {
-  const ideas = [
-    {
-      nick: 'cool-idea-nick-1',
-      name: 'Idea 1',
-      description: 'Description of idea 1...',
-    },
-    {
-      nick: 'cool-idea-nick-2',
-      name: 'Idea 2',
-      description: 'Description of idea 2...',
-    },
-    {
-      nick: 'cool-idea-nick-3',
-      name: 'Idea 3',
-      description: 'Description of idea 3...',
-    },
-  ]
   return (
-    <div>
-      <h1>Ideanick</h1>
-      {ideas.map((idea) => {
-        return (
-          <div key={idea.nick}>
-            <h2>{idea.name}</h2>
-            <p>{idea.description}</p>
-          </div>
-        )
-      })}
-    </div>
-  )
-}
+    <TrpcProvider>
+      <AllIdeasPage />
+    </TrpcProvider>
+  );
+};
--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  optimizeDeps: {
+    exclude: [
+      '@trpc/server', // Исключаем серверную зависимость, чтобы она не попадала в клиентскую сборку
+      '@trpc/server/observable',
+    ],
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        '@trpc/server', // Также можно исключить из финальной сборки
+        '@trpc/server/observable',
+      ],
+    },
+  },
+  ssr: {
+    noExternal: ['@trpc/client', '@trpc/react-query'], // важно для SSR, если используется
+  },
+});
